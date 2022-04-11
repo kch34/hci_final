84feafbba13e35e158dbeb19e772578f31a7344a
Kevin Hostler <hostlerkev@gmail.com>
2022-04-11 14:27:36 -0400

    Add files via upload
    First code file initial change

---
+++ hci_iter_2.py
@@ -0,0 +1,20 @@
+#on ubuntu sudo apt-get install python3-pil python3-pil.imagetk
+
+# import required modules
+""""
+import tkinter
+import tkinter.messagebox as msgbox
+top = tkinter.Tk()
+def helloCallBack():
+   msgbox.showinfo( "Hello Python", "Hello World")
+B = tkinter.Button(top, text ="Hello", command = helloCallBack)
+B.pack()
+top.mainloop()
+"""
+
+
+#slideshow
+import tkinter as tk
+from tkinter import *
+from PIL import Image
+from PIL import ImageTk
